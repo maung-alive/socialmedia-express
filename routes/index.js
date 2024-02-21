3164c82382e42f7cebd693098c1435d620a7920a
Maung <maung.alive@gmail.com>
2024-02-22 00:38:06 +0630

    just finish the profile page

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 const { User } = require('../models/User');
 const { Post } = require('../models/Post');
+const { Image } = require('../models/Image');
 const { Types } = require('mongoose');
 var router = express.Router();
 
@@ -13,13 +14,26 @@ router.get('/', async (req, res, next) => {
     .populate('images author')
     .exec();
 
-  res.render('index', { title: "Home", user: user, posts: posts });
+  res.render('index', { title: "NextWork - Home", user: user, posts: posts });
 });
 
 router.get('/p/:username', async (req, res, next) => {
-  let user = await User.findOne({ username: req.params.username });
-  let posts = await Post.find({ author: user._id });
-  res.render('user', { title: `Profile - ${user['username']}`, posts: posts, user: user });
+  const user = await User.findOne({ username: req.params.username });
+  if(user){
+    let tab = req.url.split('?')[1];
+    if(tab=='posts'){
+        var posts = await Post.find({ author: user._id })
+          .populate('images author')
+          .exec();
+        var photos = null;
+    }else{
+        var posts = null;
+        var photos = await Image.find({ uploader: user._id });
+    }
+    res.render('user', { title: `${user['username']} - Profile`, posts: posts, photos: photos, user: user });
+  }else{
+    res.render('error')
+  }
 });
 
 router.get('/w/:id', async (req, res, next) => {
@@ -28,7 +42,7 @@ router.get('/w/:id', async (req, res, next) => {
     let post = await Post.findOne({ _id: id })
                       .populate('author images')
                       .exec();
-    res.render('post', { title: `Post - `, post: post, error: null });
+    res.render('post', { title: `${post.author.fullname} - ${post.content.split(0, 50)}`, post: post, error: null });
   } catch(error){
     res.render('post', { title: `Post - `, post: null, error: error });
   }
