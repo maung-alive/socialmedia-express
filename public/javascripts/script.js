461e8ca08f323847bde2258c0e71438495cac1f7
Maung <maung.alive@gmail.com>
2024-02-23 19:19:05 +0630

    Post upload form fontend

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,40 +1,10 @@
-const photoViewerDIV = (url) => `
-    <div class="fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-[800px] md:h-[96vh] max-w-[100vw] h-[100vh] bg-black/20 z-50 rounded-md" id="photo-viewer">
-        <span class="fixed top-0 left-0 translate-x-4 md:-translate-x-1/2 md:-translate-y-1/2 text-4xl cursor-pointer" onClick="removePhotoViewer()">&times;</span>
-
-        <img src="${url}" alt="" class="fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-full h-auto object-contain bg-slate-500">
-    </div>
-`
-
-const commentFormDIV = (id) => `
-    <div class="fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-[700px] h-[800px] max-w-[96%] max-h-[60vh] bg-slate-100 shadow-xl rounded-xl" id="comment-form-div">
-            
-        <span class="fixed top-0 left-1/2 -translate-x-1/2 -translate-y-1/2 text-4xl rounded-full duration-200 hover:ring-1 ring-red-500 z-[100]" onClick="destroyCommenter()">&times;</span>
-
-        <div class="my-4 w-full relative px-2">
-        <textarea
-            type="text"
-            name="comment"
-            id="comment"
-            placeholder="Write a comment.."
-            class="w-full py-3 pl-2 pr-6 rounded-md bg-white "
-        >${id}</textarea>
-
-        <span class="absolute top-1/2 right-4 -translate-y-1/2 bg-cyan-700 hover:bg-cyan-800 hover:shadow-lg cursor-pointer text-white px-3 py-2 rounded-full">
-            S
-        </span>
-        </div>
-
-    </div>
-`
-
 function appendToBody(element) {
     let htmlBody = document.querySelector("body");
     htmlBody.innerHTML += element;
 }
 
 function destroyByID(id) {
-    let div = document.querySelector(`#${id}`);
+    let div = document.getElementById(id);
     div.outerHTML = "";
 }
 
@@ -48,27 +18,67 @@ function existing(selector) {
 }
 
 function photoViewer(url) {
-    if(existing('#photo-viewer')){
-        destroyByID('photo-viewer');
-    }
-    
-    let div = photoViewerDIV(url);
-    appendToBody(div);
+    let div = document.querySelector("#photo-viewer");
+    div.className = div.className.replace("hidden", "");
+    let img = document.querySelector("#photo-viewer-img");
+    img.src = url;
 }
 
 function removePhotoViewer() {
-    destroyByID('photo-viewer');
+    let div = document.querySelector("#photo-viewer");
+    div.className += " hidden";
+    let img = document.querySelector("#photo-viewer-img");
+    img.src = "";
 }
 
 function commenter(id) {
-    if(existing("#comment-form-div")) {
-        destroyByID('comment-form-div');
-    }
-
-    let div = commentFormDIV(id);
-    appendToBody(div);
+    let div = document.querySelector("#comment-form-div");
+    div.className = div.className.replace("hidden", "");
+    let form = document.querySelector("#comment-form");
+    form.setAttribute("action", `/w/${id}/comment`);
 }
 
 function destroyCommenter() {
-    destroyByID('comment-form-div');
+    let div = document.querySelector("#comment-form-div");
+    div.className += " hidden";
+    let form = document.querySelector("#comment-form");
+    form.reset();
+}
+
+const previewImageDIV = (filename) =>  `
+    <div id="${filename}" class="relative">
+        <img
+            id="${filename}-img"
+            alt="${filename}"
+            onClick="photoViewer(this.src)"
+            class="min-w-[80px] h-[80px] bg-gray-300 object-cover rounded-xl text-6xl text-center text-gray-400 cursor-pointer hover:scale-95 active:scale-95 duration-200"
+        />
+        <div class="cursor-pointer absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 border-2 border-red-600 px-2 rounded-full">
+            <span class="text-red-600 font-bold text-2xl" onclick="destroyByID('${filename}')">&times;</span>
+        </div>
+    </div>
+`
+
+function previewFile(input) {   // image previewer
+    let previewSlider = document.querySelector("#preview-slider");  // get parent div
+    let files = input.files;    // get input data
+    let i = 0;  // set i for looping
+
+    while(files.length > 0){    // do this untill there are no files left
+        let file = files[i];    // get i^th item from files
+        let reader  = new FileReader();
+
+        var element = previewImageDIV(file.name)
+        previewSlider.innerHTML += element;
+        element = document.getElementById(file.name + '-img')
+        
+        reader.onloadend = function () {
+            element.src = reader.result;
+        }
+        
+        if (file) { reader.readAsDataURL(file); }
+        else { element.src = "" }
+
+        i++; // add i to continue looping
+    }
 }
